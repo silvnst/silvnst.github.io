020b6b2ac0eeb33edee0c8a3767cf8a00dbb93dd
silvnst <55844758+silvnst@users.noreply.github.com>
2022-11-01 13:47:11 +0100

    new

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+
+(function($) {
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header > .container'),
+        $footer = $('#footer'),
+        $main = $('#main');
+
+	
+		// Breakpoints.
+        breakpoints({
+			xlarge:  [ '1281px',  '1800px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ],
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Touch?
+
+
+	// Footer.
+		breakpoints.on('<=medium', function() {
+			$footer.insertAfter($main);
+		});
+
+		breakpoints.on('>medium', function() {
+			$footer.appendTo($header);
+		});
+	
+})(jQuery);
